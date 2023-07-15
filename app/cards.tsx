e91760b42167d1e5dfb40ad176cf3ac7b784c25b
ripterdust <bryantello2010@hotmail.com>
2023-07-15 17:54:12 -0600

    main, Added the comopnent list

---
+++ app/cards.tsx
@@ -0,0 +1,7 @@
+import {CardInterface} from '@/interfaces/cardList.interface';
+
+export const cards: CardInterface[] = [
+  {
+    component: <></>
+  }
+];
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Card from '../components/Card';
+import {cards} from './cards';
+import {CardInterface} from '@/interfaces/cardList.interface';
 
 export default function Home() {
   return (
     <React.Fragment>
       <div className="cards">
-        <div className="card">hola</div>
-        <div className="card">hola</div>
+        {cards.map((card: CardInterface, key) => {
+          const {component} = card;
+          const Component = () => component;
+
+          return (
+            <Card key={key}>
+              <Component />
+            </Card>
+          );
+        })}
       </div>
     </React.Fragment>
   );
