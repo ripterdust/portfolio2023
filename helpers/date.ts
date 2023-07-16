e43c6ceff8a2dbbbd3da1b0f0344180ca360c3b8
ripterdust <bryantello2010@hotmail.com>
2023-07-15 19:04:21 -0600

    main, Added the getNow method

--- helpers/date.ts
+++ helpers/date.ts
@@ -1,9 +1,13 @@
 export class DateHelpers {
   public static getAge = (): number => {
     const birthDate = 2000;
-    const actualDate = new Date().getFullYear();
+    const actualDate = this.getNow().getFullYear();
     const dateOfBirth = new Date(birthDate, 0, 1).getFullYear();
 
     return actualDate - dateOfBirth;
+  };
+
+  public static getNow = (): Date => {
+    return new Date();
   };
 }
